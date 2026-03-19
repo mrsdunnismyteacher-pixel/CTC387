4a43d1b11dab50c4715a7fc7ee6a69021588f157
Marie Dunn <mrsdunnismyteacher@gmail.com>
2026-03-19 02:02:27 +0000

    fix

--- MDunnMT.py
+++ MDunnMT.py
@@ -8,10 +8,10 @@ y=11
 while (y != 7):
     y = int(input("Enter a number between 0 and 10 "))
     if (y<7):
-        print("Your guess is too low. Try again.")
+        print("Your guess is too low. You entered:  ", y)
     if (y>7):
-        print("Your guess is too high. Try again.")
+        print("Your guess is too high. You entered:  ", y)
     if (y ==7):
-        print("Winner, winner, chicken dinner")
+        print("You guessed correctly. You entered:  ", y)
     if (y<0) or (y>10):
         print("Your guess is not within the requested range.")
